410657d803eb7343358c3f6234b5225c9d649c91
JUSTINE UMWALI <umwalijustine37@gmail.com>
2025-02-20 23:18:43 +0200

    Add files via upload

---
+++ OOPS/example4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+int age = 30;
+float pi = 3.14;
+double num = 3.1415926535;
+char letter = 'A';
+bool isHappy = true;
+string name = "Alice";
+
+cout << "age: " << age << endl;
+cout << "pi: " << pi << endl;
+cout << "num: " << num << endl;
+cout << "letter: " << letter << endl;
+cout << "isHappy: " << isHappy << endl;
+cout << "name: " << name << endl;
+
+return 0;
+}
+
